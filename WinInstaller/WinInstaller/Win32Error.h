f5a4a13d18a7cda7fc2f276a2c33e1721c43c50b
Super Pudding <superpudding98@gmail.com>
2020-10-03 14:56:29 +0300

    Consistent messages + exception from last error

--- WinInstaller/WinInstaller/Win32Error.h
+++ WinInstaller/WinInstaller/Win32Error.h
@@ -6,16 +6,15 @@
 
 namespace mywininstaller
 {
+	// We could inherit from std::system_error and hook this up to automatically show error message when you call what(),
+	// but std::system_error can possibly overflow (it uses int for error codes instead of DWORD) and anyway the message is almost always "Unkown error".
+	// So for consistency and correctness of messages it's better to just show error code.
 	class Win32Error : public std::runtime_error
 	{
 	public:
-		Win32Error(const DWORD errorCode, const std::string& message = "");
-		Win32Error(const Win32Error& other) = default;
-		Win32Error(Win32Error&& other) = default;
-		virtual ~Win32Error() = default;
-		Win32Error& operator=(const Win32Error& other) = default;
-		Win32Error& operator=(Win32Error&& other) = default;
+		Win32Error(const DWORD errorCode);
 		DWORD getErrorCode() const;
+		static Win32Error fromLastError();
 
 	private:
 		const DWORD m_errorCode;
