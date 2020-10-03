f03480fdc9415d9c5d0ed584ce11180d77c14f5e
Super Pudding <superpudding98@gmail.com>
2020-10-03 21:21:17 +0300

    More refactoring

--- WinInstaller/WinInstaller/Win32Error.h
+++ WinInstaller/WinInstaller/Win32Error.h
@@ -5,17 +5,20 @@
 
 namespace mywininstaller
 {
-	// We could inherit from std::system_error and hook this up to automatically show error message when you call what(),
-	// but std::system_error can possibly overflow (it uses int for error codes instead of DWORD) and anyway the message is almost always "Unkown error".
-	// So for consistency and correctness of messages it's better to just show error code.
-	class Win32Error : public std::runtime_error
+	namespace winapi
 	{
-	public:
-		Win32Error(const DWORD errorCode);
-		DWORD getErrorCode() const;
-		static Win32Error fromLastError();
+		// We could inherit from std::system_error and hook this up to automatically show error message when you call what(),
+		// but std::system_error can possibly overflow (it uses int for error codes instead of DWORD) and anyway the message is almost always "Unkown error".
+		// So for consistency and correctness of messages it's better to just show error code.
+		class Win32Error : public std::runtime_error
+		{
+		public:
+			Win32Error(const DWORD errorCode);
+			DWORD getErrorCode() const;
+			static Win32Error fromLastError();
 
-	private:
-		const DWORD m_errorCode;
-	};
+		private:
+			const DWORD m_errorCode;
+		};
+	}
 }
